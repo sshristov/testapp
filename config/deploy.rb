03f8824b622d1ad6e98a95895b656ec6578b88fb
sshristov <sshristov@gmail.com>
2014-05-27 15:13:33 +0300

    test neznam si koi 555566

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ set :format, :pretty
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
- set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -45,14 +45,14 @@ set :format, :pretty
 namespace :deploy do
 
   desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+  #task :restart do
+  #  on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-       execute :touch, release_path.join('tmp/restart.txt')
-    end
+ #      execute :touch, release_path.join('tmp/restart.txt')
+ #   end
   end
 
-  after :publishing, :restart
+  #after :publishing, :restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
