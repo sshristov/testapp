63a8952fbf38bdf0219a9e86187a6f16ce522f8d
sshristov <sshristov@gmail.com>
2014-05-27 17:12:08 +0300

    test neznam si koi 2010

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,8 +61,8 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
        #Here we can do anything such as:
          within release_path do
-         execute :rake, 'db:migrate' 'rails s'
-	
+         execute :rake, 'db:migrate' #'rails s'
+	 execute :rails, 's'
 	 #execute 'rails s'
        end
     end
