9a9eff98d9901ac7a3b19757762d98ac2cb504f8
sshristov <sshristov@gmail.com>
2014-05-27 11:25:02 +0300

    Test 3

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,7 +60,8 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
-         execute :rake db:migrate, 'cache:clear'
+         execute :rake db, 'cache:clear'
+	 execute :rake db, 'db:migrate'
        end
     end
   end
