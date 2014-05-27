55364bcf807b4495ea4eb8cd142a74f47984be25
sshristov <sshristov@gmail.com>
2014-05-27 16:11:04 +0300

    test neznam si koi 99999999

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,13 +53,6 @@ namespace :deploy do
     end
   end
 
- task :start do ; end
- task :stop do ; end
- task :restart, :roles => :app, :except => { :no_release => true } do
-   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
- end
-
-
   after :publishing, :restart
 
   after :restart, :clear_cache do
