4c14e7e6eb91bc3ffb7f5f8e770dd357cfba82b5
sshristov <sshristov@gmail.com>
2014-05-27 17:09:24 +0300

    test neznam si koi 2008

--- config/deploy.rb
+++ config/deploy.rb
@@ -68,14 +68,15 @@ namespace :deploy do
     end
   end
   
-  after :restart, :runtestapp
-  after :runtestapp, :clear_cache do
+  after :publishing, :restart
+  
+  after :restart :clear_cache do
    on roles(:web), in: :groups, limit: 3, wait: 10 do
      #Here we can do anything such as:
        #within release_path do
        #execute :rake, 'db:migrate'
        within release_path do
-        execute 'rails s'
+       execute 'rails s'
        end
      end
   end
