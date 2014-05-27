0b4c9f914d4eb31034d9ed24f0c2b975a2b5693e
sshristov <sshristov@gmail.com>
2014-05-27 15:33:31 +0300

    test neznam si koi 555574

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,15 +44,15 @@ set :format, :pretty
 
 namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
+#  desc 'Restart application'
+#  task :restart do
+#    on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-       execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
+#       execute :touch, release_path.join('tmp/restart.txt')
+#    end
+#  end
 
-  after :publishing, :restart
+#  after :publishing, :restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
