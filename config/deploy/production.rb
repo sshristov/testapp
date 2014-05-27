dc9d8bad4240e3abdf67f65f934f3cd72f54d46a
sshristov <sshristov@gmail.com>
2014-05-27 10:53:00 +0300

    Monkata Bonbonkata

---
+++ config/deploy/production.rb
@@ -0,0 +1,57 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+role :app, %w{deployer@10.0.11.14}
+role :web, %w{deployer@10.0.11.14}
+role :db,  %w{deployer@10.0.11.14}
+
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
+
+server '10.0.11.14', user: 'deployer', roles: %w{web app}, my_property: :my_value
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# Global options
+# --------------
+  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+    forward_agent: false,
+    auth_methods: %w(password),
+    password: 'Ana86Nas',
+    user: 'deployer',
+  }
+
+
+	
+#    or per server (overrides global)
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
