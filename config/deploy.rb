ffd66705a2b43786c4c6b36b20e9c42671f7408c
sshristov <sshristov@gmail.com>
2014-05-27 15:08:08 +0300

    test neznam si koi 555565

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,8 +57,8 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
        #Here we can do anything such as:
-       within release_path do
-         execute :rake, 'cache:clear'
+         within release_path do
+         execute :rake, 'db:migrate'
        end
     end
   end
