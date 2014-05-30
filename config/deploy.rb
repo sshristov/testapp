e3c5349e5adecdf05199750d13e36e08a0025b99
sshristov <sshristov@gmail.com>
2014-05-30 13:33:32 +0300

    testFineal2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,71 +1,34 @@
-# config valid only for Capistrano 3.1
-lock '3.2.1'
-
-set :application, 'testapp'
-set :repo_url, 'git@github.com:sshristov/testapp.git'
-
-set :deploy_to, "/home/deployer/apps/testapp"
-
-set :pty, true
-
-set :format, :pretty
-
-
-#Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-
+set :application, "testapp"
+set :repository,  "git://github.com/sshristov/testapp.git"
+ 
+set :user, :deployer
+ 
+set :deploy_to, "/home/deployer/apps/#{application}"
+ 
+set :use_sudo, false
+ 
+set :scm, :git
+ 
+role :web, "10.0.11.14"                          # Your HTTP server, Apache/etc
+role :app, "10.0.11.14"                          # This may be the same as your `Web` server
+role :db,  "10.0.11.14", :primary => true # This is where Rails migrations will run
+role :db,  "10.0.11.14"
+ 
+default_run_options[:pty] = true
+ 
 namespace :deploy do
-
-  desc 'Restart application'
-
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      #execute :touch, 'tmp/restart.txt'
-      execute :touch, release_path.join('/tmp/testapp/restart.txt')
-      
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-       #Here we can do anything such as:
-         within release_path do
-         execute :rake, 'db:migrate' #'rails s'
-	 #execute :rails, 'server -d'
-	 #execute 'rails s'
-       end
-    end
-  end
-  
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ 
+   desc "Installs required gems"
+   task :gems, :roles => :app do
+     run "cd #{current_path} && sudo rake gems:install RAILS_ENV=production"
+   end
+   after "deploy:setup", "deploy:gems"  
+ 
+   before "deploy", "deploy:web:disable"
+   after "deploy", "deploy:web:enable"
 end
