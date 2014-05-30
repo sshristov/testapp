d4546d58ddc29df8e241a547a377446ef2420da2
sshristov <sshristov@gmail.com>
2014-05-30 15:31:43 +0300

    testFineal27

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,8 +26,8 @@ namespace :deploy do
       # Your restart mechanism here, for example:
       #execute :touch, 'tmp/restart.txt'
       execute :touch, release_path.join('/tmp/testapp/restart.txt')
-      #execute :kill, '-9', cat('tmp/pids/server.pid')
-      run "kill -9 $(cat tmp/pids/server.pid)"
+      execute :kill, '-9', 'cat 'tmp/pids/server.pid'
+      #run "kill -9 $(cat tmp/pids/server.pid)"
     end
   end
 
